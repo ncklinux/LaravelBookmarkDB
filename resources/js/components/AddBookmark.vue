bcda6d4d66146742ad850262e0554ee7b8a8ded6
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-03 19:08:51 +0100

    feat: test typescript in new component

---
+++ resources/js/components/AddBookmark.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        <div class="card-header">Add new bookmark</div>
+        <div class="card-body">Example component {{ getUser(user) }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            user: <User>{
+                name: "Test",
+                email: "test@test.local",
+                password: "Test",
+            },
+        };
+    },
+    mounted() {
+        console.log("Component mounted.");
+    },
+    methods: {
+        getUser(user: User) {
+            console.log("Function with type: ", user);
+        },
+    },
+};
+</script>
