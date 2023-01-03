720ee023b58492158a4232ad68f314a936ad918a
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-03 11:14:28 +0100

    fix: adjust grid columns in resources

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,14 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="card">
+        <div class="card-header">Add new bookmark</div>
+        <div class="card-body">Example component.</div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
 </script>
