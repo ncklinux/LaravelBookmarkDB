bd9be8d9213db93fb6a5d8c2210072d72984027c
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-04 18:03:29 +0100

    feat: import bootstrap icons

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,8 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-import './bootstrap';
-import { createApp } from 'vue';
+import "./bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { createApp } from "vue";
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
@@ -36,4 +37,4 @@ app.component('example-component', ExampleComponent);
  * scaffolding. Otherwise, you will need to add an element yourself.
  */
 
-app.mount('#app');
+app.mount("#app");
