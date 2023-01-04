ff223522d4e4661c72e34b709668ea28db551448
Nick <68464552+ncklinux@users.noreply.github.com>
2023-01-04 18:05:33 +0100

    feat: add translations for components

---
+++ lang/en/components.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Components Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in Vue components for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    'new_folder' => [
+        'title' => 'Add folder',
+        'color' => 'Color',
+        'folder' => 'Folder name',
+        'description' => 'Description',
+        'button' => 'Add folder',
+    ],
+    'new_bookmark' => [
+        'title' => 'Add bookmark',
+        'color' => 'Color',
+        'bookmark' => 'Bookmark name',
+        'description' => 'Description',
+        'button' => 'Add bookmark',
+    ],
+
+];
